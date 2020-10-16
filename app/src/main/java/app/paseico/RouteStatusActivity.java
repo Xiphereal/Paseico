2081a95768747e123fff99a34e00376ad58f8875
Franciscoggonzalvez <f.garciagonzalvez@gmail.com>
2020-10-16 16:19:59 +0200

    Se añade RouteStatusActivity que es la pantalla de la app que permite ver el estado de la Ruta y se enlaza con RoutingActivity a través de los POIs

---
+++ app/src/main/java/app/paseico/RouteStatusActivity.java
@@ -0,0 +1,50 @@
+package app.paseico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.location.Location;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class RouteStatusActivity extends AppCompatActivity {
+    static ArrayList<String> pointsOfInterests = new ArrayList<String>();
+    //static ArrayList<LatLng> locations = new ArrayList<LatLng>();
+    static ArrayAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_status);
+
+        ListView listView = findViewById(R.id.listView);
+        pointsOfInterests.add("POI 1");
+        pointsOfInterests.add("POI 2");
+        pointsOfInterests.add("POI 3");
+        //locations.add(new LatLng(0,0));
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pointsOfInterests);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), RoutingActivity.class);
+                intent.putExtra("placeNumber",i);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/app/paseico/RoutingActivity.java
+++ app/src/main/java/app/paseico/RoutingActivity.java
@@ -2,7 +2,9 @@ package app.paseico;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,18 +27,12 @@ public class RoutingActivity extends FragmentActivity implements OnMapReadyCallb
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        Intent intent = getIntent();
+        Toast.makeText(this, Integer.toString(intent.getIntExtra("placeNumber", 0)), Toast.LENGTH_SHORT).show();
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
