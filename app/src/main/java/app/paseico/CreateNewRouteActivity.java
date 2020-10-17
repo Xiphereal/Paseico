0b4135640afaebfbbb4a67502f68f0b6f2cb3720
Alejandro Lozano <lozanojalex@gmail.com>
2020-10-17 19:52:46 +0200

    Añadido el soporte básico para el botón de finilizar creación de ruta

--- app/src/main/java/app/paseico/CreateNewRouteActivity.java
+++ app/src/main/java/app/paseico/CreateNewRouteActivity.java
@@ -1,8 +1,11 @@
 package app.paseico;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import app.paseico.data.PointOfInterest;
+import app.paseico.data.Route;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,6 +14,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +23,7 @@ import java.util.List;
 public class CreateNewRouteActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap createNewRouteMap;
-    private List<PointOfInterest> pointsSelected = new ArrayList<>();
+    private List<PointOfInterest> selectedPointsOfInterest = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,8 @@ public class CreateNewRouteActivity extends AppCompatActivity implements OnMapRe
         setContentView(R.layout.activity_create_new_route);
 
         initializeMapFragment();
+
+        registerFinalizeRouteCreationButtonTransition();
     }
 
     private void initializeMapFragment() {
@@ -35,19 +42,81 @@ public class CreateNewRouteActivity extends AppCompatActivity implements OnMapRe
         mapFragment.getMapAsync(this);
     }
 
+    private void registerFinalizeRouteCreationButtonTransition() {
+        ExtendedFloatingActionButton extendedFloatingActionButton = findViewById(R.id.finalize_route_creation_button);
+        extendedFloatingActionButton.setOnClickListener(view -> tryFinalizeRouteCreation());
+    }
+
+    private void tryFinalizeRouteCreation() {
+        TextInputEditText textInputEditText = findViewById(R.id.route_name_textInputEditText);
+
+        // TODO: Store the route in Firebase.
+        Route newRoute = new Route(textInputEditText.getText().toString(), selectedPointsOfInterest);
+
+        // TODO: When the conditions are not met, we must show an error message and just close the dialog on click OK.
+        showConfirmationDialog();
+    }
+
+    private void showConfirmationDialog() {
+        // Where the alert dialog is going to be shown.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // TODO: Extract the string to a resources file or similar abstraction.
+        builder.setMessage("La nueva ruta ha sido guardada satisfactoriamente.")
+                .setTitle("Finalizar creación de ruta")
+                .setPositiveButton("OK", (dialog, which) -> {
+                    // Take the user back to the main map activity
+                    Intent goToMainMapIntent = new Intent(getApplicationContext(), MainMapActivity.class);
+                    startActivity(goToMainMapIntent);
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        createNewRouteMap = googleMap;
+
+        addFakePOIsToMap(createNewRouteMap);
+
+        LatLng fakeUserPosition = new LatLng(39.475, -0.375);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(fakeUserPosition));
+
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+        registerOnMarkerClickListener();
+    }
+
+    private void addFakePOIsToMap(GoogleMap googleMap) {
+        LatLng valenciaCathedral = new LatLng(39.475139, -0.375372);
+        googleMap.addMarker(new MarkerOptions().position(valenciaCathedral).title("Cathedral"));
+
+        LatLng albertosBar = new LatLng(39.471958, -0.370947);
+        googleMap.addMarker(new MarkerOptions().position(albertosBar).title("Alberto's bar"));
+
+        LatLng torresSerrano = new LatLng(39.479063, -0.376115);
+        googleMap.addMarker(new MarkerOptions().position(torresSerrano).title("Torres de Serrano"));
+
+        LatLng ayuntamiento = new LatLng(39.469734, -0.376868);
+        googleMap.addMarker(new MarkerOptions().position(ayuntamiento).title("Ayuntamiento"));
+
+        LatLng gulliver = new LatLng(39.462987, -0.359719);
+        googleMap.addMarker(new MarkerOptions().position(gulliver).title("Gulliver"));
+    }
+
     private void registerOnMarkerClickListener() {
         createNewRouteMap.setOnMarkerClickListener(marker -> {
-
             PointOfInterest poi = findClickedPointOfInterest(marker);
 
             if (isPointOfInterestSelected(poi)) {
                 marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
 
-                pointsSelected.remove(poi);
+                selectedPointsOfInterest.remove(poi);
             } else {
                 marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
 
-                pointsSelected.add(new PointOfInterest(marker, marker.getId()));
+                selectedPointsOfInterest.add(new PointOfInterest(marker, marker.getId()));
             }
 
             return true;
@@ -55,7 +124,7 @@ public class CreateNewRouteActivity extends AppCompatActivity implements OnMapRe
     }
 
     private PointOfInterest findClickedPointOfInterest(Marker marker) {
-        for (PointOfInterest poi : pointsSelected) {
+        for (PointOfInterest poi : selectedPointsOfInterest) {
             if (poi.getGoogleMarker().equals(marker))
                 return poi;
         }
@@ -65,29 +134,5 @@ public class CreateNewRouteActivity extends AppCompatActivity implements OnMapRe
 
     private boolean isPointOfInterestSelected(PointOfInterest poi) {
         return poi != null;
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        createNewRouteMap = googleMap;
-        addFakePOIsToMap(createNewRouteMap);
-        LatLng fakeUserPosition = new LatLng(39.475,-0.375);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(fakeUserPosition));
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
-
-        registerOnMarkerClickListener();
-    }
-
-    private void addFakePOIsToMap(GoogleMap googleMap) {
-        LatLng valenciaCathedral = new LatLng(39.475139, -0.375372);
-        googleMap.addMarker(new MarkerOptions().position(valenciaCathedral).title("Cathedral"));
-        LatLng albertosBar = new LatLng(39.471958, -0.370947);
-        googleMap.addMarker(new MarkerOptions().position(albertosBar).title("Alberto's bar"));
-        LatLng torresSerrano = new LatLng(39.479063, -0.376115);
-        googleMap.addMarker(new MarkerOptions().position(torresSerrano).title("Torres de Serrano"));
-        LatLng ayuntamiento = new LatLng(39.469734, -0.376868);
-        googleMap.addMarker(new MarkerOptions().position(ayuntamiento).title("Ayuntamiento"));
-        LatLng gulliver = new LatLng(39.462987, -0.359719);
-        googleMap.addMarker(new MarkerOptions().position(gulliver).title("Gulliver"));
     }
 }
