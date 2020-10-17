619b620b6831e463c3b03f3a694f3f034eaa979a
Víctor García Valero <garciavicval@gmail.com>
2020-10-17 17:31:24 +0200

    Nuevos POI main map

--- app/src/main/java/app/paseico/CreateNewRouteActivity.java
+++ app/src/main/java/app/paseico/CreateNewRouteActivity.java
@@ -41,15 +41,25 @@ public class CreateNewRouteActivity extends AppCompatActivity implements OnMapRe
         });
     }
 
+    private void addFakePOIsToMap(GoogleMap googleMap) {
+        LatLng valenciaCathedral = new LatLng(39.475139, -0.375372);
+        googleMap.addMarker(new MarkerOptions().position(valenciaCathedral).title("Cathedral"));
+        LatLng albertosBar = new LatLng(39.471958, -0.370947);
+        googleMap.addMarker(new MarkerOptions().position(albertosBar).title("Alberto's bar"));
+        LatLng torresSerrano = new LatLng(39.479063, -0.376115);
+        googleMap.addMarker(new MarkerOptions().position(torresSerrano).title("Torres de Serrano"));
+        LatLng ayuntamiento = new LatLng(39.469734, -0.376868);
+        googleMap.addMarker(new MarkerOptions().position(ayuntamiento).title("Ayuntamiento"));
+        LatLng gulliver = new LatLng(39.462987, -0.359719);
+        googleMap.addMarker(new MarkerOptions().position(gulliver).title("Gulliver"));
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         createNewRouteMap = googleMap;
-        LatLng valenciaCathedral = new LatLng(39.47, -0.38);
-        googleMap.addMarker(new MarkerOptions().position(valenciaCathedral).title("Cathedral"));
-        LatLng albertosBar = new LatLng(39.47, -0.37);
-        googleMap.addMarker(new MarkerOptions().position(albertosBar).title("Alberto's bar"));
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(valenciaCathedral));
+        addFakePOIsToMap(createNewRouteMap);
+        LatLng fakeUserPosition = new LatLng(39.475,-0.375);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(fakeUserPosition));
         googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
 
         registerOnMarkerClickListener();
