204fae24cc0c43ef4b25603521f6a5ad32fc6fd0
magre1 <magre1@inf.upv.es>
2020-10-18 00:53:14 +0200

    Rellenados los Spinners de tematica, longitud y duración

--- app/src/main/java/app/paseico/SearchFragment.java
+++ app/src/main/java/app/paseico/SearchFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,13 +13,30 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class SearchFragment extends Fragment {
 
+    EditText et_keyWord;
+    EditText et_numberOfPOI;
+    EditText et_minimumOfPoints;
+    Spinner spinner_theme;
+    Spinner spinner_length;
+    Spinner spinner_estimatedTime;
+
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View fragmentSearchLayout = inflater.inflate(R.layout.fragment_search, container, false);
+
+        et_keyWord = fragmentSearchLayout.findViewById(R.id.editText_keyWord);
+        et_numberOfPOI = fragmentSearchLayout.findViewById(R.id.editText_numberOfPOI);
+        et_minimumOfPoints = fragmentSearchLayout.findViewById(R.id.editText_minimumOfPoints);
+        spinner_theme = fragmentSearchLayout.findViewById(R.id.spinner_route_theme);
+        spinner_length = fragmentSearchLayout.findViewById(R.id.spinner_route_length);
+        spinner_estimatedTime = fragmentSearchLayout.findViewById(R.id.spinner_route_estimated_time);
+
+        return fragmentSearchLayout;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -26,6 +45,7 @@ public class SearchFragment extends Fragment {
         view.findViewById(R.id.btn_search).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 NavHostFragment.findNavController(SearchFragment.this)
                         .navigate(R.id.action_SearchFragment_to_RouteListFragment);
             }
