2cee6bf41aea73564799e00d0d0a02e1b4dc1187
magre1 <magre1@inf.upv.es>
2020-10-17 17:19:26 +0200

    Añadidos fragmentos para implementar las pantallas de la funcionalidad
    SearchFragment corresponde a la pantalla de búsqueda de rutas. RouteListFragment es el resultado de la búsqueda

---
+++ app/src/main/java/app/paseico/SearchFragment.java
@@ -0,0 +1,34 @@
+package app.paseico;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class SearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.search_fragment, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SearchFragment.this)
+                        .navigate(R.id.action_SearchFragment_to_RouteListFragment);
+            }
+        });
+    }
+}
