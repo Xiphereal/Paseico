bcfe28b81da3b761de4ff8f486b33266dcfcd4b9
RICARDO SOLER BACHS <risobac@upv.edu.es>
2020-10-17 14:14:01 +0200

    Añadida ventana inicial con el logo de la aplicación

---
+++ app/src/main/java/app/paseico/OpeningActivity.java
@@ -0,0 +1,13 @@
+package app.paseico;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpeningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+    }
+}
