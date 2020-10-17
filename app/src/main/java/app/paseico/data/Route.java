4cced795a1a954abbc1eb22954fc195edb9dfa50
magre1 <magre1@inf.upv.es>
2020-10-17 16:08:36 +0200

    Añadidos atributos a Route
    Estos atributos servirán para filtrar en la búsqueda

--- app/src/main/java/app/paseico/data/Route.java
+++ app/src/main/java/app/paseico/data/Route.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Route {
     private String name;
+    private String theme;   // The value of theme should be a constant.
+    private double length;  // Meters.
+    private int estimatedTime;  // Minutes
+    private int points;     // Points earned when the route is completed.
     private List<PointOfInterest> pointsOfInterest;
 
     public String getName() {
@@ -13,6 +17,38 @@ public class Route {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public int getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public void setEstimatedTime(int estimatedTime) {
+        this.estimatedTime = estimatedTime;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
     }
 
     public List<PointOfInterest> getPointsOfInterest() {
