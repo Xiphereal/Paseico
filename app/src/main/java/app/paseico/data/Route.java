1e20d3fab91039201f3d9e429177473ae814cd9c
magre1 <magre1@inf.upv.es>
2020-10-21 00:17:03 +0200

    Renombrado Points a Reward Points

--- app/src/main/java/app/paseico/data/Route.java
+++ app/src/main/java/app/paseico/data/Route.java
@@ -1,27 +1,35 @@
 package app.paseico.data;
 
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class Route {
+public class Route implements Parcelable {
     private String name;
     private String theme;   // TODO :The value of theme should be a constant.
     private double length;  // Meters.
     private double estimatedTime;  // Minutes
-    private int points;     // Points earned when the route is completed.
+    private int rewardPoints;     // Points earned when the route is completed.
     private List<PointOfInterest> pointsOfInterest;
 
+    public Route(Parcel in){
+        readFromParcel(in);
+    }
     public Route(String name, List<PointOfInterest> pointOfInterests) {
         this.name = name;
         this.pointsOfInterest = pointOfInterests;
     }
 
+    public Route(){
+    }
     public Route(String name, String theme, double length, double estimatedTime, int points, List<PointOfInterest> pointsOfInterest) {
         this.name = name;
         this.theme = theme;
         this.length = length;
         this.estimatedTime = estimatedTime;
-        this.points = points;
+        this.rewardPoints = points;
         this.pointsOfInterest = pointsOfInterest;
     }
 
@@ -58,11 +66,11 @@ public class Route {
     }
 
     public int getPoints() {
-        return points;
+        return rewardPoints;
     }
 
     public void setPoints(int points) {
-        this.points = points;
+        this.rewardPoints = points;
     }
 
     public List<PointOfInterest> getPointsOfInterest() {
@@ -71,5 +79,55 @@ public class Route {
 
     public void setPointsOfInterest(List<PointOfInterest> pointsOfInterest) {
         this.pointsOfInterest = pointsOfInterest;
+    }
+
+    @Override
+    public String toString() {
+        return "Route{" +
+                "name='" + name + '\'' +
+                ", theme='" + theme + '\'' +
+                ", length=" + length +
+                ", estimatedTime=" + estimatedTime +
+                ", rewardsPoints=" + rewardPoints +
+                ", pointsOfInterest=" + pointsOfInterest +
+                '}';
+    }
+
+    private void readFromParcel(Parcel in){
+        this.name = in.readString();
+        this.theme = in.readString();
+        this.length = in.readDouble();
+        this.estimatedTime = in.readDouble();
+        this.rewardPoints = in.readInt();
+        this.pointsOfInterest = in.readArrayList(PointOfInterest.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<Route> CREATOR =  new Parcelable.Creator<Route>(){
+        @Override
+        public Route createFromParcel(Parcel in){
+            return new Route(in);
+
+        }
+
+        @Override
+        public Route[] newArray(int size) {
+            return new Route[0];
+        }
+    };
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(theme);
+        dest.writeDouble(length);
+        dest.writeDouble(estimatedTime);
+        dest.writeInt(rewardPoints);
+        dest.writeList(pointsOfInterest);
     }
 }
