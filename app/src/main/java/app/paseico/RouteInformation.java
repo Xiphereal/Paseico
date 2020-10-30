36cbaf8ea5849c0603575e15d0f76ec9aaf2fab8
magre1 <magre1@inf.upv.es>
2020-10-30 19:05:32 +0100

    Creada interfaz de la informacion de ruta

---
+++ app/src/main/java/app/paseico/RouteInformation.java
@@ -0,0 +1,14 @@
+package app.paseico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RouteInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_information);
+    }
+}
