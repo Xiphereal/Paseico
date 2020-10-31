48c89a982f81cb5da824b95809aaa6168ab2c23b
Alejandro Lozano <lozanojalex@gmail.com>
2020-11-01 00:48:52 +0100

    Añadida nueva entrada a la lista de Fragments del Drawer

--- app/src/main/java/app/paseico/MainMenuActivity.java
+++ app/src/main/java/app/paseico/MainMenuActivity.java
@@ -1,20 +1,16 @@
 package app.paseico;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -42,7 +38,7 @@ public class MainMenuActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_profile, R.id.nav_routes, R.id.nav_searcher)
+                R.id.nav_profile, R.id.nav_create_new_route, R.id.nav_routes, R.id.nav_searcher)
                 .setDrawerLayout(drawer)
                 .build();
     }
