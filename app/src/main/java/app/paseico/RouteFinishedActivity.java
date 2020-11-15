f33b388cf051fa6c393e0bc8f8cec92decda5318
smugylooky <matiascuda@gmail.com>
2020-11-15 21:21:44 +0100

    Rework del manifest
    Los puntos ahora se suman a la cantidad actual del usuario
    (Pendiente: resolver asincronia para problemas de seguridad)

--- app/src/main/java/app/paseico/RouteFinishedActivity.java
+++ app/src/main/java/app/paseico/RouteFinishedActivity.java
@@ -1,5 +1,6 @@
 package app.paseico;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,11 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import app.paseico.data.PointOfInterest;
 import app.paseico.data.Route;
+import app.paseico.data.User;
 import app.paseico.login.OpeningActivity;
 
 public class RouteFinishedActivity extends AppCompatActivity {
@@ -21,10 +32,34 @@ public class RouteFinishedActivity extends AppCompatActivity {
     String nombreRuta;
     List<PointOfInterest> nombrePOIs;
     int rewpoints;
+    private DatabaseReference myUsersRef = FirebaseDatabase.getInstance().getReference("users"); //Node users reference
+    private User user = new User();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseUser fbusr = firebaseAuth.getCurrentUser();
+    private DatabaseReference myActualUserRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //goToMenu.setClickable(false);
         Route route = (Route) getIntent().getExtras().get("route");
+        myActualUserRef = myUsersRef.child(fbusr.getUid());
+        myActualUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                user = snapshot.getValue(User.class);
+                int points = route.getRewardPoints();
+                int actualpoints = user.getPoints();
+                int updatedpoints = points + actualpoints;
+                final DatabaseReference mypointsreference = myActualUserRef.child("points");
+                mypointsreference.setValue(updatedpoints);
+                //goToMenu.setClickable(true);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         intent = getIntent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_finished);
@@ -52,7 +87,6 @@ public class RouteFinishedActivity extends AppCompatActivity {
 
 
         //listaPOIS.setText(listado);
-
         Button goToMenu = findViewById(R.id.buttonTemporalMenuStartRoute);
         goToMenu.setOnClickListener(new View.OnClickListener() {
             @Override
