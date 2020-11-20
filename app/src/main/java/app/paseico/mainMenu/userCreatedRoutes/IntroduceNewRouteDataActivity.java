e1bce9a686eb232b942964e2192f81411fb9614d
Alejandro Lozano <lozanojalex@gmail.com>
2020-11-20 23:44:16 +0100

    Añadida nueva actividad para introducir los datos de la nueva Ruta

---
+++ app/src/main/java/app/paseico/mainMenu/userCreatedRoutes/IntroduceNewRouteDataActivity.java
@@ -0,0 +1,14 @@
+package app.paseico.mainMenu.userCreatedRoutes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import app.paseico.R;
+
+public class IntroduceNewRouteDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduce_new_route_data);
+    }
+}
