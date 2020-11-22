8c69037958265fea1fdd7673849272e4e8a12be4
JoseRom99 <71761010+JoseRom99@users.noreply.github.com>
2020-11-22 17:32:14 +0100

    Mas cambios
    Mas cambios

--- app/src/main/java/app/paseico/login/LogInActivity.java
+++ app/src/main/java/app/paseico/login/LogInActivity.java
@@ -42,10 +42,12 @@ public class LogInActivity extends AppCompatActivity {
     static final int GOOGLE_SIGN = 123;
     private FirebaseAuth firebaseAuth;
     private SignInButton googleSignInButton;
-    private EditText etEmail, etPassword;
+    private EditText etEmail,
+            etPassword;
     private GoogleSignInClient googleSignInClient;
     private UserDao userDao = new UserDao();
-    private Button routerBtn, organiBtn;
+    private Button routerBtn,
+            organiBtn;
     int purple = R.color.purple;
 
     @Override
@@ -53,28 +55,25 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-
-
-
         etEmail = findViewById(R.id.editTextEmail);
         etPassword = findViewById(R.id.editTextPassword);
         Button btnLogin = findViewById(R.id.buttonLogIn);
 
-        btnLogin.setOnClickListener(view -> SignInPaseico());
+        btnLogin.setOnClickListener(view ->SignInPaseico());
 
         googleSignInButton = findViewById(R.id.sign_in_button);
-        googleSignInButton.setOnClickListener(v -> SignInGoogle());
+        googleSignInButton.setOnClickListener(v ->SignInGoogle());
 
         TextView register = findViewById(R.id.textViewRegister);
 
         //On click you go to the register form
-        register.setOnClickListener(view -> {
+        register.setOnClickListener(view ->{
             Intent intent = new Intent();
-            if(!routerBtn.isEnabled()) {
-                 intent = new Intent(LogInActivity.this, RegisterActivity.class);
+            if (!isRouterTabSelected()) {
+                intent = new Intent(LogInActivity.this, RegisterActivity.class);
             }
-            else{
-                 intent = new Intent(LogInActivity.this, RegisterOrganizationActivity.class);
+            else {
+                intent = new Intent(LogInActivity.this, RegisterOrganizationActivity.class);
             }
             startActivity(intent);
             finish();
@@ -82,11 +81,7 @@ public class LogInActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
-                .Builder()
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder().requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
 
         googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
 
@@ -97,26 +92,24 @@ public class LogInActivity extends AppCompatActivity {
         organiBtn.setEnabled(true);
         organiBtn.setBackgroundColor(Color.DKGRAY);
 
-        routerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                googleSignInButton.setVisibility(View.VISIBLE);
-                routerBtn.setEnabled(false);
-                routerBtn.setBackgroundColor(getResources().getColor(purple));
-                organiBtn.setEnabled(true);
-                organiBtn.setBackgroundColor(Color.DKGRAY);
-            }
+        routerBtn.setOnClickListener(new View.OnClickListener() {@Override
+        public void onClick(View view) {
+            googleSignInButton.setVisibility(View.VISIBLE);
+            routerBtn.setEnabled(false);
+            routerBtn.setBackgroundColor(getResources().getColor(purple));
+            organiBtn.setEnabled(true);
+            organiBtn.setBackgroundColor(Color.DKGRAY);
+        }
         });
 
-        organiBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                googleSignInButton.setVisibility(View.GONE);
-                routerBtn.setEnabled(true);
-                routerBtn.setBackgroundColor(Color.DKGRAY);
-                organiBtn.setEnabled(false);
-                organiBtn.setBackgroundColor(getResources().getColor(purple));
-            }
+        organiBtn.setOnClickListener(new View.OnClickListener() {@Override
+        public void onClick(View view) {
+            googleSignInButton.setVisibility(View.GONE);
+            routerBtn.setEnabled(true);
+            routerBtn.setBackgroundColor(Color.DKGRAY);
+            organiBtn.setEnabled(false);
+            organiBtn.setBackgroundColor(getResources().getColor(purple));
+        }
         });
     }
 
@@ -126,60 +119,22 @@ public class LogInActivity extends AppCompatActivity {
     }
 
     private void SignInPaseico() {
-        String email =etEmail.getText().toString();
-        String pass =etPassword.getText().toString();
-        if(email.matches("") || pass.matches("")){
-            Toast.makeText(LogInActivity.this, "Faltan campos por rellenar!",
-                Toast.LENGTH_SHORT).show();
+        String email = etEmail.getText().toString();
+        String pass = etPassword.getText().toString();
+        if (email.matches("") || pass.matches("")) {
+            Toast.makeText(LogInActivity.this, "Faltan campos por rellenar!", Toast.LENGTH_SHORT).show();
         }
         else {
-            firebaseAuth.signInWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString())
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("TAG", "signInWithEmail:success");
-
-
-                            FirebaseUser user = task.getResult().getUser();
-                            DatabaseReference mUsersReference = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
-                            DatabaseReference mOrganizationsReference = FirebaseDatabase.getInstance().getReference("organizations").child(user.getUid());
-
-                            ValueEventListener eventListener = new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    if (!dataSnapshot.exists()) {
-                                        if (!routerBtn.isEnabled()) {
-                                            Toast.makeText(LogInActivity.this, "ERROR :Estas intentando acceder como Organización desde el apartado de Rutero",
-                                                    Toast.LENGTH_SHORT).show();
-                                        } else {
-                                            Toast.makeText(LogInActivity.this, "ERROR :Estas intentando acceder como  Rutero desde el apartado de Organización",
-                                                    Toast.LENGTH_SHORT).show();
-                                        }
-
-                                        FirebaseAuth.getInstance().signOut();
-                                    } else {
-                                        goToMainScreen();
-                                    }
-                                }
-
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-
-                                }
-                            };
-                            if (!routerBtn.isEnabled()) {
-                                mUsersReference.addListenerForSingleValueEvent(eventListener);
-                            } else {
-                                mOrganizationsReference.addListenerForSingleValueEvent(eventListener);
-                            }
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("TAG", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LogInActivity.this, "Correo electronico o contraseña incorrectos!",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
+            firebaseAuth.signInWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString()).addOnCompleteListener(this, task ->{
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    checkCorrectTabUser(task.getResult().getUser());
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.getException());
+                    Toast.makeText(LogInActivity.this, "Correo electronico o contraseña incorrectos!", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
@@ -187,16 +142,14 @@ public class LogInActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GOOGLE_SIGN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn
-                    .getSignedInAccountFromIntent(data);
+            Task < GoogleSignInAccount > task = GoogleSignIn.getSignedInAccountFromIntent(data);
 
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
 
-                if (account != null)
-                    firebaseAuthWithGoogle(account);
+                if (account != null) firebaseAuthWithGoogle(account);
 
-            } catch (ApiException e) {
+            } catch(ApiException e) {
                 e.printStackTrace();
             }
         }
@@ -204,54 +157,88 @@ public class LogInActivity extends AppCompatActivity {
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
         Log.d("TAG", "firebaseAuthWithGoogle: " + account.getId());
-        AuthCredential credential = GoogleAuthProvider
-                .getCredential(account.getIdToken(), null);
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
 
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        FirebaseUser firebaseAuthCurrentUser = firebaseAuth.getCurrentUser();
-                        Log.d("TAG", "Signin success");
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, task ->{
+            if (task.isSuccessful()) {
+                FirebaseUser firebaseAuthCurrentUser = firebaseAuth.getCurrentUser();
+                Log.d("TAG", "Signin success");
 
-                        boolean isNewUser = task.getResult().getAdditionalUserInfo().isNewUser();
+                boolean isNewUser = task.getResult().getAdditionalUserInfo().isNewUser();
 
-                        if (isNewUser)
-                            registerNewUserToDatabase(account, task, firebaseAuthCurrentUser);
-                        else
-                            goToMainScreen();
+                if (isNewUser) registerNewUserToDatabase(account, task, firebaseAuthCurrentUser);
+                else goToMainScreen();
 
-                    } else {
-                        Log.w("TAG", "Signin failed", task.getException());
+            } else {
+                Log.w("TAG", "Signin failed", task.getException());
 
-                        Toast.makeText(this, "SingIn Failed!", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                Toast.makeText(this, "SingIn Failed!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    private void registerNewUserToDatabase(GoogleSignInAccount account, Task<AuthResult> task, FirebaseUser user) {
+    private void registerNewUserToDatabase(GoogleSignInAccount account, Task < AuthResult > task, FirebaseUser user) {
         Log.w("TAG", "NEW USER", task.getException());
         Log.w("TAG", user.getUid(), task.getException());
 
         //Wait 2 secs to load the next activity (LoginScreen)
         Handler handler = new Handler();
-        handler.postDelayed(() -> {
-            Log.w("TAG", "GOOOOOOOOOOOOGLE", task.getException());
-            userDao.addGoogleUser(user, account.getDisplayName());
-            goToMainScreen();
-        }, 3000);
+        handler.postDelayed(() ->{
+                    Log.w("TAG", "GOOOOOOOOOOOOGLE", task.getException());
+                    userDao.addGoogleUser(user, account.getDisplayName());
+                    goToMainScreen();
+                },
+                3000);
     }
-
-
 
     private void goToMainScreen() {
         Intent intent = new Intent();
-        if(!routerBtn.isEnabled()) {
+        if (!isRouterTabSelected()) {
             intent = new Intent(LogInActivity.this, MainMenuActivity.class);
         }
-        else{
+        else {
             intent = new Intent(LogInActivity.this, MainMenuOrganizationActivity.class);
         }
         startActivity(intent);
         finish();
+    }
+
+    private boolean isRouterTabSelected() {
+        if (routerBtn.isEnabled()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void checkCorrectTabUser(FirebaseUser user) { //CHECKS IF THE USER YOU ARE TRYING TO LOG IN MATCHES WITH THE SELECTED TAB (ROUTER/ORGANIZATION)
+        DatabaseReference mUsersReference = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+        DatabaseReference mOrganizationsReference = FirebaseDatabase.getInstance().getReference("organizations").child(user.getUid());
+
+        ValueEventListener eventListener = new ValueEventListener() {@Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            if (!dataSnapshot.exists()) {
+                if (!isRouterTabSelected()) {
+                    Toast.makeText(LogInActivity.this, "ERROR :Estas intentando acceder como Organización desde el apartado de Rutero", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LogInActivity.this, "ERROR :Estas intentando acceder como  Rutero desde el apartado de Organización", Toast.LENGTH_SHORT).show();
+                }
+
+                FirebaseAuth.getInstance().signOut();
+            } else {
+                goToMainScreen();
+            }
+        }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        if (!isRouterTabSelected()) {
+            mUsersReference.addListenerForSingleValueEvent(eventListener);
+        } else {
+            mOrganizationsReference.addListenerForSingleValueEvent(eventListener);
+        }
     }
 }
--- app/src/main/java/app/paseico/login/RegisterOrganizationActivity.java
+++ app/src/main/java/app/paseico/login/RegisterOrganizationActivity.java
@@ -31,7 +31,11 @@ import app.paseico.data.Router;
 import app.paseico.data.UserDao;
 
 public class RegisterOrganizationActivity extends AppCompatActivity {
- private EditText etName, etPass, etPassConf, etNif, etMail;
+    private EditText etName,
+            etPass,
+            etPassConf,
+            etNif,
+            etMail;
     private Button btnRegister;
     private FirebaseAuth mAuth;
     String name = null;
@@ -56,67 +60,32 @@ public class RegisterOrganizationActivity extends AppCompatActivity {
 
         btnRegister = findViewById(R.id.buttonOrganiRegister);
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkRegister();
-            }
+        btnRegister.setOnClickListener(new View.OnClickListener() {@Override
+        public void onClick(View view) {
+            checkRegister();
+        }
         });
     }
 
-    private void checkRegister(){
-
+    private void checkRegister() {
         name = etName.getText().toString();
         email = etMail.getText().toString();
         password = etPass.getText().toString();
         passwordConf = etPassConf.getText().toString();
         nif = etNif.getText().toString();
 
-        if(!TextUtils.isEmpty(etName.getText().toString())
-                &&!TextUtils.isEmpty(etMail.getText().toString())
-                &&!TextUtils.isEmpty(etPass.getText().toString())
-                &&!TextUtils.isEmpty(etPassConf.getText().toString())
-                &&!TextUtils.isEmpty(etNif.getText().toString())
-        ){ //Check that the fields aren't empty
-            if(password.length()>=6) {
+        if (areAllFormFieldsFIlled()) { //Check that the fields aren't empty
+            if (password.length() >= 6) {
                 if (password.equals(passwordConf)) { //Check if passwords match
-                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-                    DatabaseReference userNameRef = ref.child("organizations");
-                    Query queries = userNameRef.orderByChild("name").equalTo(name);
-                    ValueEventListener eventListener = new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            if(name.contains(".") || name.contains("#") || name.contains("$") || name.contains("[") || name.contains(".]") ){
-                                Toast.makeText(RegisterOrganizationActivity.this, "Error: No puedes usar un nombre con los siguientes caracteres: '.'  '# ' '$'  '['  ']' ",
-                                        Toast.LENGTH_SHORT).show();
-                            }else {
-                                if (!dataSnapshot.exists()) {
-                                    //create new user
-                                    submitRegister(name, nif, email, password);
-                                } else {
-                                    Context context = getApplicationContext();
-                                    CharSequence text = "El nombre de organización ya existe!";
-                                    int duration = Toast.LENGTH_SHORT;
-
-                                    Toast toast = Toast.makeText(context, text, duration);
-                                    toast.show();
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {}
-                    };
-                    queries.addListenerForSingleValueEvent(eventListener);
+                    checkAndRegister();
                 } else { //Passwords doesn't match
                     Context context = getApplicationContext();
                     CharSequence text = "Las contraseñas no coinciden";
                     int duration = Toast.LENGTH_SHORT;
-
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 }
-            }else{
+            } else {
                 Context context = getApplicationContext();
                 CharSequence text = "La contraseña debe contener mínimo 6 caracteres";
                 int duration = Toast.LENGTH_SHORT;
@@ -134,40 +103,69 @@ public class RegisterOrganizationActivity extends AppCompatActivity {
         }
     }
 
-    private void submitRegister(String name, String nif, String email, String password){
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+    private void submitRegister(String name, String nif, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener < AuthResult > () {@Override
+        public void onComplete(@NonNull Task < AuthResult > task) {
 
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Organization newOrganization = new Organization(name, email, name, nif);
-                            myOrganizationsRef.child(user.getUid()).setValue(newOrganization);
-                            Toast.makeText(RegisterOrganizationActivity.this, "Registro completado!",
-                                    Toast.LENGTH_SHORT).show();
-                            Handler handler = new Handler();
-                            handler.postDelayed(new Runnable() { //Wait 2 secs to load the next activity (LoginScreen)
-                                @Override
-                                public void run() {
-                                    try {
-                                        FirebaseAuth.getInstance().signOut();
-                                        Intent intent = new Intent(RegisterOrganizationActivity.this, LogInActivity.class);
-                                        startActivity(intent);
-                                        finish();
-                                    } catch (Exception e) {
-                                    }
-                                }
-                            }, 2000);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(RegisterOrganizationActivity.this, "Error: El correo electrónico ya existe",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                FirebaseUser user = mAuth.getCurrentUser();
+                Organization newOrganization = new Organization(name, email, name, nif);
+                myOrganizationsRef.child(user.getUid()).setValue(newOrganization);
+                Toast.makeText(RegisterOrganizationActivity.this, "Registro completado!", Toast.LENGTH_SHORT).show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() { //Wait 2 secs to load the next activity (LoginScreen)
+                                        @Override
+                                        public void run() {
+                                            try {
+                                                FirebaseAuth.getInstance().signOut();
+                                                Intent intent = new Intent(RegisterOrganizationActivity.this, LogInActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            } catch(Exception e) {}
+                                        }
+                                    },
+                        2000);
+            } else {
+                // If sign in fails, display a message to the user.
+                Toast.makeText(RegisterOrganizationActivity.this, "Error: El correo electrónico ya existe", Toast.LENGTH_SHORT).show();
+            }
+        }
 
+        });
+    }
 
-                });
+    private boolean areAllFormFieldsFIlled() {
+        return ! TextUtils.isEmpty(etName.getText().toString()) && !TextUtils.isEmpty(etMail.getText().toString()) && !TextUtils.isEmpty(etPass.getText().toString()) && !TextUtils.isEmpty(etPassConf.getText().toString()) && !TextUtils.isEmpty(etNif.getText().toString());
+
+    }
+
+    private void checkAndRegister() { //Checks for illegal caracters and if there is no illegal characters submits the register of the organization
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference userNameRef = ref.child("organizations");
+        Query queries = userNameRef.orderByChild("name").equalTo(name);
+        ValueEventListener eventListener = new ValueEventListener() {@Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            if (name.contains(".") || name.contains("#") || name.contains("$") || name.contains("[") || name.contains(".]")) {
+                Toast.makeText(RegisterOrganizationActivity.this, "Error: No puedes usar un nombre con los siguientes caracteres: '.'  '# ' '$'  '['  ']' ", Toast.LENGTH_SHORT).show();
+            } else {
+                if (!dataSnapshot.exists()) {
+                    //create new user
+                    submitRegister(name, nif, email, password);
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "El nombre de organización ya existe!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        queries.addListenerForSingleValueEvent(eventListener);
     }
 }
