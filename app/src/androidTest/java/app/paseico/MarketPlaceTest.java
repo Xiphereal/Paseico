65007ee2d64e6d13d90833a0016e7c86e3f3bf87
magre1 <manugreg99@gmail.com>
2020-11-24 23:10:08 +0100

    Añadida cmaretPlaceTest

---
+++ app/src/androidTest/java/app/paseico/MarketPlaceTest.java
@@ -0,0 +1,35 @@
+package app.paseico;
+
+import androidx.fragment.app.Fragment;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import app.paseico.login.LogInActivity;
+import app.paseico.login.RegisterActivity;
+import app.paseico.mainMenu.marketplace.marketplaceFragment;
+
+import static androidx.test.InstrumentationRegistry.getTargetContext;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class MarketPlaceTest {
+//    @Rule
+//    public FragmentScenario<marketplaceFragment> activityRule =
+//            new FragmentScenario<marketplaceFragment>(LogInActivity.class);
+
+}
