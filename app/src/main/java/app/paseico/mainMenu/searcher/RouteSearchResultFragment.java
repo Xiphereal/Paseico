1f5733ae9b347d2a6ddfd96f47fd54a0da72b5da
Franciscoggonzalvez <f.garciagonzalvez@gmail.com>
2020-11-26 21:48:18 +0100

    incorporada la categoria en FilteredListAdapter

--- app/src/main/java/app/paseico/mainMenu/searcher/RouteSearchResultFragment.java
+++ app/src/main/java/app/paseico/mainMenu/searcher/RouteSearchResultFragment.java
@@ -26,7 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class RouteSearchResultFragment extends Fragment {
 
-    Integer ImageName = R.drawable.notheme_icon;
+
 
     @Override
     public View onCreateView(
@@ -46,9 +46,8 @@ public class RouteSearchResultFragment extends Fragment {
         List<String> filteredRoutesLength = new ArrayList<>();
         List<String> filteredRoutesRewardPoints = new ArrayList<>();
 
-        //
         List<Integer> filteredRoutesIcons = new ArrayList<>();
-        //
+
 
         for (Route route : filteredRoutes) {
             filteredRoutesNames.add(route.getName());
@@ -62,12 +61,12 @@ public class RouteSearchResultFragment extends Fragment {
             int index = CategoryManager.ConvertCategoryToIntDrawable(RouteCategory);
             System.out.println("indice " + index);
             filteredRoutesIcons.add(index);
-            //
+
         }
 
         ListView listView_filteredRoutes = (ListView) view.findViewById(R.id.listView_filteredRoutes);
 
-        FilteredListAdapter adapter = new FilteredListAdapter(this.getContext(), filteredRoutesNames, filteredRoutesEstimatedTime, filteredRoutesLength, filteredRoutesRewardPoints, ImageName);
+        FilteredListAdapter adapter = new FilteredListAdapter(this.getContext(), filteredRoutesNames, filteredRoutesEstimatedTime, filteredRoutesLength, filteredRoutesRewardPoints, filteredRoutesIcons);
         listView_filteredRoutes.setAdapter(adapter);
 
         // Open RouteInfo screen.
@@ -91,9 +90,10 @@ public class RouteSearchResultFragment extends Fragment {
         List<String> estimatedTimes;
         List<String> lengths;
         List<String> points;
-        Integer imageName;
+        List<Integer> icons;
 
-        FilteredListAdapter(Context context, List<String> names, List<String> estimatedTimes, List<String> lengths, List<String> points, Integer imageName) {
+
+        FilteredListAdapter(Context context, List<String> names, List<String> estimatedTimes, List<String> lengths, List<String> points, List<Integer> icons) {
             super(context, R.layout.item_route_search, R.id.routeName, names);
 
             this.context = context;
@@ -101,7 +101,7 @@ public class RouteSearchResultFragment extends Fragment {
             this.estimatedTimes = estimatedTimes;
             this.lengths = lengths;
             this.points = points;
-            this.imageName = imageName;
+            this.icons = icons;
         }
 
         @NonNull
@@ -115,17 +115,14 @@ public class RouteSearchResultFragment extends Fragment {
             TextView myLengths = row.findViewById(R.id.routeLenght);
             TextView myPoints = row.findViewById(R.id.routeReward);
 
-            //ImageView ListViewImage = (ImageView) row.findViewById(R.id.imageView1);
             ImageView ListViewImage = (ImageView) row.findViewById(R.id.imageViewIcon);
-
-            System.out.println(ListViewImage + "listbiew imageeeeeeeeeeeeeeeeeeee");
 
             myNames.setText(names.get(position));
             myEstimatedTimes.setText(estimatedTimes.get(position));
             myLengths.setText(lengths.get(position));
             myPoints.setText(points.get(position));
 
-           ListViewImage.setImageResource(imageName);
+           ListViewImage.setImageResource(icons.get(position));
 
             return row;
         }
