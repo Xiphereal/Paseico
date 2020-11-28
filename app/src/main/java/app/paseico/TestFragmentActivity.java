0a93583776f94e7a29f6a879d4123e8e01a05acb
Franciscoggonzalvez <f.garciagonzalvez@gmail.com>
2020-11-28 13:16:02 +0100

    creada testFragmentActivity

---
+++ app/src/main/java/app/paseico/TestFragmentActivity.java
@@ -0,0 +1,14 @@
+package app.paseico;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class TestFragmentActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_fragment2);
+    }
+}
