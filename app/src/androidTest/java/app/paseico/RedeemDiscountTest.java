e708b3519ac166d57f5e661450dec2ce662cb962
Franciscoggonzalvez <f.garciagonzalvez@gmail.com>
2020-11-28 13:40:16 +0100

    añadido testRule

--- app/src/androidTest/java/app/paseico/RedeemDiscountTest.java
+++ app/src/androidTest/java/app/paseico/RedeemDiscountTest.java
@@ -1,14 +1,13 @@
 package app.paseico;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.Test;
+import com.android21buttons.fragmenttestrule.FragmentTestRule;
+
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import app.paseico.mainMenu.marketplace.DiscountsFragment;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,6 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 
 public class RedeemDiscountTest {
+
+    @Rule
+    public FragmentTestRule<?, DiscountsFragment> fragmentTestRule =
+            FragmentTestRule.create(DiscountsFragment.class);
 
 
 }
