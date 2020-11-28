61772cfab8b20271b7f9f406a7e50197fbe0bc86
Víctor García Valero <garciavicval@gmail.com>
2020-11-28 21:01:24 +0100

    Mierda hasta el cuello y nadie hace na
    y no tengo nada de na, duermo con eso siempre debajo de la almuada

--- app/src/androidTest/java/app/paseico/CreateNewRouteRouterTest.java
+++ app/src/androidTest/java/app/paseico/CreateNewRouteRouterTest.java
@@ -6,6 +6,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.core.AllOf;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -27,6 +28,7 @@ import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 
@@ -42,8 +44,10 @@ public class CreateNewRouteRouterTest {
         Router routerForTesting = new Router("Test","xXTesterXx","testing@test.ts");
         routerForTesting.setPoints(1000);
         List<PointOfInterest> pois4Testing = new ArrayList<>();
-        //pois4Testing.
-        //Route routeToCreate = new Route("route4Testing",,routerForTesting);
+        pois4Testing.add(new PointOfInterest(20.0,20.0,"a"));
+        pois4Testing.add(new PointOfInterest(21.0,21.0,"b"));
+        pois4Testing.add(new PointOfInterest(22.0,22.0,"c"));
+        //Route routeToCreate = new Route("route4Testing", pois4Testing, routerForTesting);
         //Log in with the local user?
 //        onView(withId(R.id.editTextEmail))
 //                .perform(typeText("userForTesting@gmail.com"), closeSoftKeyboard());
@@ -57,11 +61,20 @@ public class CreateNewRouteRouterTest {
     public void createRouteSuccessfullyTest(){
         onView(withId(R.id.route_name_textInputEditText)).perform(typeText("myRoute"),closeSoftKeyboard());
         onView(withId(R.id.finalize_route_creation_button)).perform(click());
+        onView(withText("Finalizar creación de ruta")).perform(click());
+        onView(withText("Finalizar creación de ruta")).check(matches(isDisplayed()));
+        //onView(withId(R.id.route_name_textInputEditText)).check(matches(allOf(withText("La nueva ruta ha sido guardada satisfactoriamente."),isDisplayed())));
+        //onView(withText("La nueva ruta ha sido guardada satisfactoriamente.")).check(matches(isDisplayed()));
     }
 
     @Test
     public void createRouteNotSuccessfullyTest(){
-
+        onView(withId(R.id.route_name_textInputEditText)).perform(typeText("myRoute"),closeSoftKeyboard());
+        //need pois so the cost is greater than the user's amount of point
+        //onView(withId(R.id.map)).perform(click());
+        onView(withId(R.id.finalize_route_creation_button)).perform(click());
+        onView(withText("Finalizar creación de ruta")).perform(click());
+        onView(withText("No tienes los puntos suficientes para crear la ruta.")).check(matches(isDisplayed()));
     }
 
     @Test
