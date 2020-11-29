379e6996151c2358e4a626139f1a29ca34c51bfe
Alejandro Lozano <lozanojalex@gmail.com>
2020-11-29 17:13:00 +0100

    Reemplazado el uso de concatenación de Strings por una StringBuilder

--- app/src/main/java/app/paseico/service/DistanceMatrixRequest.java
+++ app/src/main/java/app/paseico/service/DistanceMatrixRequest.java
@@ -2,19 +2,26 @@ package app.paseico.service;
 
 import android.content.Context;
 import app.paseico.R;
+import app.paseico.data.PointOfInterest;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class DistanceMatrixRequest {
 
-    private String urlRequest = "https://maps.googleapis.com/maps/api/distancematrix/";
+    private StringBuilder urlRequest;
 
     private final Context context;
 
+    @NotNull
     public DistanceMatrixRequest(Context context) {
         this.context = context;
+
+        urlRequest = new StringBuilder("https://maps.googleapis.com/maps/api/distancematrix/json?");
     }
 
     public void send() {
-        urlRequest += "&key=" + context.getString(R.string.google_api_key);
+        urlRequest.append("&key=").append(context.getString(R.string.google_api_key));
 
         // TODO: Send the request to Google.
     }
