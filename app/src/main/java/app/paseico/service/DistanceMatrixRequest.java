d67c52297992ac8ae64f5df531d89694036859af
Alejandro Lozano <lozanojalex@gmail.com>
2020-11-29 17:13:56 +0100

    Añadida la posibilidad de añadir la colección de Puntos de Interés de la Ruta a la petición para la Distance Matrix API

--- app/src/main/java/app/paseico/service/DistanceMatrixRequest.java
+++ app/src/main/java/app/paseico/service/DistanceMatrixRequest.java
@@ -20,6 +20,31 @@ public class DistanceMatrixRequest {
         urlRequest = new StringBuilder("https://maps.googleapis.com/maps/api/distancematrix/json?");
     }
 
+    /**
+     * Adds the first {@link PointOfInterest} in the passed collection as an origin, the last as the
+     * destination and any other {@link PointOfInterest} as a waypoint for the route.
+     */
+    public DistanceMatrixRequest addPointsOfInterest(List<PointOfInterest> pointsOfInterest) {
+        urlRequest.append("origins=");
+        appendPointOfInterest(pointsOfInterest.get(0));
+
+        urlRequest.append("&waypoints=");
+        for (int i = 1; i < pointsOfInterest.size() - 1; i++) {
+            appendPointOfInterest(pointsOfInterest.get(i));
+        }
+
+        urlRequest.append("&destinations=");
+        appendPointOfInterest(pointsOfInterest.get(pointsOfInterest.size()));
+
+        return this;
+    }
+
+    private void appendPointOfInterest(PointOfInterest poi) {
+        urlRequest.append(poi.getLatitude());
+        urlRequest.append(",");
+        urlRequest.append(poi.getLongitude());
+    }
+
     public void send() {
         urlRequest.append("&key=").append(context.getString(R.string.google_api_key));
 
