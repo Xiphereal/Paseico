58ed3c68adda750d2a78422e3ebc0b156aaaf748
Alejandro Lozano <lozanojalex@gmail.com>
2020-11-29 15:34:31 +0100

    Refactorizaciones varias sobre FirebaseService

--- app/src/main/java/app/paseico/service/FirebaseService.java
+++ app/src/main/java/app/paseico/service/FirebaseService.java
@@ -17,9 +17,9 @@ import java.util.List;
 
 public class FirebaseService {
 
-    private static FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private static FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-    private static FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private static final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private static final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private static final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
 
     public static FirebaseUser getCurrentUser() {
         return firebaseAuth.getCurrentUser();
@@ -29,52 +29,39 @@ public class FirebaseService {
         return firebaseDatabase.getReference("users").child(getCurrentUser().getUid());
     }
 
-    public static String saveRoute(Route route) {
-        firebaseFirestore.collection("route").add(route).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                String createdRouteID = documentReference.getId();
-                updateRoute(createdRouteID, "id", createdRouteID);
-            }
+    public static void saveRoute(Route route) {
+        firebaseFirestore.collection("route").add(route).addOnSuccessListener(documentReference -> {
+            String createdRouteID = documentReference.getId();
+            updateRoute(createdRouteID, "id", createdRouteID);
         });
 
-        return "Route " + route.getName() + " succesfully added to Firebase.";
+        System.out.println("Route " + route.getName() + " successfully added to Firebase.");
     }
 
-    public static String updateRoute(String routeId, String attribute, String newValue) {
-        return FirebaseService.updateRouteObject(routeId, attribute,newValue);
+    public static void updateRoute(String routeId, String attribute, String newValue) {
+        updateRouteObject(routeId, attribute, newValue);
     }
 
-    public static String updateRoute(String routeId, String attribute, Double newValue) {
-        return FirebaseService.updateRouteObject(routeId, attribute,newValue);
+    public static void updateRoute(String routeId, String attribute, Double newValue) {
+        updateRouteObject(routeId, attribute, newValue);
     }
 
-    public static String updateRoute(String routeId, String attribute, int newValue) {
-        return FirebaseService.updateRouteObject(routeId, attribute,newValue);
+    public static void updateRoute(String routeId, String attribute, int newValue) {
+        updateRouteObject(routeId, attribute, newValue);
     }
 
-    public static String updateRoute(String routeId, String attribute, List<PointOfInterest> newValue) {
-        return FirebaseService.updateRouteObject(routeId, attribute,newValue);
+    public static void updateRoute(String routeId, String attribute, List<PointOfInterest> newValue) {
+        updateRouteObject(routeId, attribute, newValue);
     }
 
-    private static String updateRouteObject(String routeId, String attribute, Object newValue) {
+    private static void updateRouteObject(String routeId, String attribute, Object newValue) {
         DocumentReference reference = firebaseFirestore.collection("route").document(routeId);
 
         reference
                 .update(attribute, newValue)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d("Succes", "DocumentSnapshot successfully updated!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("Failure", "Error updating document", e);
-                    }
-                });
+                .addOnSuccessListener(aVoid -> Log.d("Succes", "DocumentSnapshot successfully updated!"))
+                .addOnFailureListener(e -> Log.w("Failure", "Error updating document", e));
 
-        return "Updated " + attribute + " as " + newValue.toString() ;
+        System.out.println("Updated " + attribute + " as " + newValue.toString());
     }
 }
