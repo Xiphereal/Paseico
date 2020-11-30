929e127a4bb0f57acf652cc3ddad1e8849394602
Alejandro Lozano <lozanojalex@gmail.com>
2020-12-01 00:01:33 +0100

    Marcadas variables y métodos de instancia como estáticos para que puedan ser llamados en los @beforeClass y @afterClass

--- app/src/androidTest/java/app/paseico/RouteInformationTest.java
+++ app/src/androidTest/java/app/paseico/RouteInformationTest.java
@@ -5,9 +5,7 @@ import app.paseico.data.PointOfInterest;
 import app.paseico.data.Route;
 import app.paseico.mainMenu.searcher.RouteSearchFragment;
 import app.paseico.service.FirebaseService;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,28 +16,31 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class RouteInformationTest {
-    private final String expectedRouteName = "TestRoute";
-    private final String expectedTheme = "Museos";
-    private final double expectedLength = 200;
-    private final double expectedEstimatedTime = 5;
-    private final int expectedRewardPoints = 150;
-    private final List<PointOfInterest> expectedPointsOfInterest = new ArrayList<>();
+    private static final String expectedRouteName = "TestRoute";
+    private static final String expectedTheme = "Museos";
+    private static final double expectedLength = 200;
+    private static final double expectedEstimatedTime = 5;
+    private static final int expectedRewardPoints = 150;
+    private static final List<PointOfInterest> expectedPointsOfInterest = new ArrayList<>();
 
-    private Route expectedRoute;
+    private static Route expectedRoute;
 
     @Rule
     public FragmentTestRule<?, RouteSearchFragment> fragmentTestRule =
             FragmentTestRule.create(RouteSearchFragment.class);
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void beforeClass() {
         createRoute();
+    }
 
+    @Before
+    public void beforeEach() {
         fragmentTestRule.getActivity()
                 .getSupportFragmentManager().beginTransaction();
     }
 
-    private void createRoute() {
+    private static void createRoute() {
         expectedPointsOfInterest.add(new PointOfInterest(39.66290265223034, -0.25573253631591797, "testPoi1"));
         expectedPointsOfInterest.add(new PointOfInterest(39.47360355556778, -0.37898540496826166, "testPoi2"));
         expectedPointsOfInterest.add(new PointOfInterest(39.47596848025821, -0.3752410411834717, "testPoi3"));
@@ -62,5 +63,14 @@ public class RouteInformationTest {
         onView(withId(R.id.btn_search)).perform(click());
 
         onView(withText(expectedRouteName)).perform(click());
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        deleteExpectedRouteFromDatabase();
+    }
+
+    private static void deleteExpectedRouteFromDatabase() {
+        FirebaseService.deleteRoute(expectedRoute);
     }
 }
